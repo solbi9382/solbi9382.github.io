afbd13468eab7d3d7267b45f8c8ae88dc02c79cd
SolbiShin <sbshin.dev@gmail.com>
2022-07-26 23:30:42 +0900

    개발 환경 세팅

---
+++ blog/gatsby-node.js
@@ -0,0 +1,16 @@
+const path = require("path")
+
+exports.createPages = async ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  })
+}
