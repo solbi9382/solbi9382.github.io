f0900651fcb8d38d6f516f86fb96de2a3189e696
SolbiShin <sbshin.dev@gmail.com>
2022-07-26 23:43:15 +0900

    개발 환경 세팅

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,15 +1,26 @@
-const path = require("path")
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions
+  // createPage({
+  //   path: '/using-dsg',
+  //   component: require.resolve('./src/templates/using-dsg.js'),
+  //   context: {},
+  //   defer: true,
+  // })
+}
 
-exports.createPages = async ({ getConfig, actions }) => {
+const path = require('path')
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
   const output = getConfig().output || {}
 
   actions.setWebpackConfig({
     output,
     resolve: {
       alias: {
-        components: path.resolve(__dirname, "src/components"),
-        utils: path.resolve(__dirname, "src/utils"),
-        hooks: path.resolve(__dirname, "src/hooks"),
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
       },
     },
   })
