a2e44e0c03af10ab15a01ab1dae0617c8ee249e1
SolbiShin <sbshin.dev@gmail.com>
2022-07-28 21:25:14 +0900

    게시물 목록 컴포넌트 구현

---
+++ blog/src/components/Main/PostList.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import PostItem from './PostItem'
+
+const POST_ITEM_DATA = {
+  title: 'Post Item',
+  date: '2022.07.20',
+  categories: ['React', 'Javascript', 'Testing'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    'https://images.unsplash.com/photo-1656618724305-a4257e46e847?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80',
+  link: '<https://www.google.co.kr/>',
+}
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+`
+
+const PostList: FunctionComponent = function () {
+  return (
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
+  )
+}
+
+export default PostList
