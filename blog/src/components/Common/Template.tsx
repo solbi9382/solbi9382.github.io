ed4d8256b604f0182237fba48002cb8a45ccadba
SolbiShin <sbshin.dev@gmail.com>
2022-08-10 23:09:01 +0900

    메타 태그 추가 작업중

--- blog/src/components/Common/Template.tsx
+++ blog/src/components/Common/Template.tsx
@@ -2,7 +2,13 @@ import React, { FunctionComponent, ReactNode } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Footer from 'components/Common/Footer'
+import { Helmet } from 'react-helmet'
+
 type TemplateProp = {
+  title: string
+  description: string
+  url: string
+  image: string
   children: ReactNode
 }
 
@@ -11,9 +17,33 @@ const Container = styled.div`
   flex-direction: column;
   height: 100%;
 `
-const Template: FunctionComponent<TemplateProp> = function ({ children }) {
+const Template: FunctionComponent<TemplateProp> = function ({
+  title,
+  description,
+  url,
+  image,
+  children,
+}) {
   return (
     <Container>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:url" content={url} />
+        <meta property="og:site_name" content={title} />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+      </Helmet>
       <GlobalStyle />
       {children}
       <Footer />
