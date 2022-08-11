0e8dac79e56605eb734ee4ecd7cc4762125faff6
SolbiShin <sbshin.dev@gmail.com>
2022-08-12 00:27:52 +0900

    원복

---
+++ blog/src/components/Post/CommentWidget.tsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled'
+import React, { createRef, FunctionComponent, useEffect } from 'react'
+
+const src = 'https://utteranc.es/client.js'
+const repo = 'solbi9382/solbi9382.github.io'
+
+type UtterancesAttributesType = {
+  src: string
+  repo: string
+  'issue-term': string
+  label: string
+  theme: string
+  crossorigin: string
+  async: string
+}
+
+const UtteranceWrapper = styled.div`
+  @media (max-width: 768px) {
+    padding: 0 20px;
+  }
+`
+
+const CommentWidget: FunctionComponent = function () {
+  const element = createRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (element.current === null) return
+    const utterances: HTMLScriptElement = document.createElement('script')
+
+    const attributes: UtterancesAttributesType = {
+      src,
+      repo,
+      'issue-term': 'pathname',
+      label: 'Comment',
+      theme: `github-light`,
+      crossorigin: 'anonymous',
+      async: 'true',
+    }
+    Object.entries(attributes).forEach(([key, value]) => {
+      utterances.setAttribute(key, value)
+    })
+    element.current.appendChild(utterances)
+  }, [])
+  return <UtteranceWrapper ref={element} />
+}
+
+export default CommentWidget
